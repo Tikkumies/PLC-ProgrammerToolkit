24c406733a1618f703a2e6520e574868e6946308
Tikkumies <mikko_stick@hotmail.com>
2023-02-21 19:11:13 +0200

    Check mark added to manubar

--- resources/mainwindow.py
+++ resources/mainwindow.py
@@ -16,22 +16,29 @@ class MainWindow(QMainWindow):
         self.stacked_widget.addWidget(self.ip_window)
         self.stacked_widget.addWidget(self.test_window)
         self.setCentralWidget(self.stacked_widget)
-        # menu bar
+        # menubar
         self.menu = QMenuBar()
         self.setMenuBar(self.menu)
         file_menu = self.menu.addMenu("&View")
-        self.action_ip_view = QAction("&IP changer", self)
-        self.action_test_view = QAction("&Delta conversion", self)
+        self.action_ip_view = QAction("& IP changer", self)
+        self.action_ip_view.setCheckable(True)
+        self.action_ip_view.setChecked(True)
+        self.action_delta_view = QAction("& Delta conversion", self)
+        self.action_delta_view.setCheckable(True)
         file_menu.addAction(self.action_ip_view)
-        file_menu.addAction(self.action_test_view)
+        file_menu.addAction(self.action_delta_view)
         # signals
         self.action_ip_view.triggered.connect(self.open_ip_changer)
-        self.action_test_view.triggered.connect(self.open_delta_conversion)
+        self.action_delta_view.triggered.connect(self.open_delta_conversion)
 
     def open_ip_changer(self):
         self.stacked_widget.setCurrentIndex(0)
         self.setWindowTitle("IP changer")
+        self.action_ip_view.setChecked(True)
+        self.action_delta_view.setChecked(False)
 
     def open_delta_conversion(self):
         self.stacked_widget.setCurrentIndex(1)
         self.setWindowTitle("Delta conversion")
+        self.action_delta_view.setChecked(True)
+        self.action_ip_view.setChecked(False)
