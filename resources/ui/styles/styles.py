90f7cb6431f64b22815167957af18e6c08855a8d
Tikkumies <mikko_stick@hotmail.com>
2023-02-21 18:57:01 +0200

    Menubar styling changes

--- resources/ui/styles/styles.py
+++ resources/ui/styles/styles.py
@@ -54,13 +54,45 @@ GLOBAL_STYLE = """
     QWidget#myParentWidget{
         background-color: #121212;
     }
+
     QMenuBar {
+        font-size: 16px;
+        color: #FFFFFF;
+        background-color: qlineargradient(x1:0, y1:0, x2:0, y2:0,
+                                        stop:0 #404040 , stop:1 #404040);
+        spacing: 3px;
+    }
+
+    QMenuBar::item {
+        padding: 1px 4px;
         background-color: #404040;
+        border-radius: 4px;
+    }
+
+    QMenuBar::item:selected {
+        background-color: #181818;
+    }
+
+    QMenuBar::item:pressed {
+        background-color: #B2B2B2;
+    }
+
+    QMenu{
+        background-color: #404040;
+        spacing: 3px;
+        color: #FFFFFF
+
+    }
+    QMenu::item{
+        background-color: transparent;
+        font-size: 16px;
+        padding: 6px;
+
+    }
+
+    QMenu::item::selected{
+        background-color: #181818;
         color: #FFFFFF;
         font-size: 16px;
-    }
-    QLabel#DeltaGuiTitle{
-        font-weight: bold;
-        font-size: 20px;
     }
     """
